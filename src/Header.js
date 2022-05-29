69331dd92544a823de644628a9da672facb9870b
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2022-05-29 18:13:35 -0300

    Desafio clase n2

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+function Header (){
+    return(
+        <header>
+            <div className="Title">
+                <h1>Africa Clothing</h1>
+
+            </div>
+            <div className="navbar">
+                <a href="#">Home</a>
+                <a href="#">contact</a>
+                <a href="#">About us</a>
+            </div>
+            <div className="navbutt">
+                <button>Log in</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
